27665659dce10d8f32dfa5d4409d5627833c853c
Nasywa Namira Suhendro <nasywa.namira@ui.ac.id>
2026-09-01 17:50:09 +0700

    tes

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -148,3 +148,5 @@ WHITENOISE_USE_FINDERS = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+## komen buat push
