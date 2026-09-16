3ca5e46a35e7a0c6e9cb04b861e6a9984f7eb9ea
Nasywa Namira Suhendro <nasywa.namira@ui.ac.id>
2026-09-16 21:26:45 +0700

    Tutorial 03: JSON & XML

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_education, create_project
+from main.views import show_main, show_experience, show_education, create_project, show_projects, get_projects_json, delete_project
 
 app_name = "main" # memberikan namespace pada URL milik aplikasi main.
 
@@ -9,6 +9,9 @@ urlpatterns = [
     path("experience/", show_experience, name="show_experience"), # experience
     path("education/", show_education, name="show_education"), # education
     path("projects/add/", create_project, name="create_project"), #project
+    path("projects/", show_projects, name="show_projects"),
+    path("api/projects/", get_projects_json, name="get_projects_json"),
+    path("projects/<uuid:project_id>/delete/",delete_project,name="delete_project"),
 ]
 
 # Pola URL "" berarti halaman utama aplikasi tanpa tambahan path.
